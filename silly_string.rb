001bcef160fccbca8997597cd8da13568fd4847f
AllisonR5 <allicraft05@gmail.com>
2018-07-24 14:14:32 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,4 +1,6 @@
 puts "What is your favorite word?" 
 word = gets.chomp
 puts "I love the word #{word}!"
-
+puts "What is your favorite number?"
+number = gets.chomp 
+puts "The number #{number} is a cool number!"
